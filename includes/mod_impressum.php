97ac397cf5e1a189bc1b71150af5bdf2a053e8c6
tobilektri <tobilektri@yahoo.de>
2016-10-06 18:09:59 +0200

    Create mod_impressum.php

---
+++ includes/mod_impressum.php
@@ -0,0 +1,160 @@
+<h1>Impressum</h1>
+wachplan.info ist ein Projekt von IT-Meindl
+<h2>Angaben gemäß § 5 TMG:</h2>
+Tobias Meindl
+IT Meindl
+Germanenweg 2
+85774 Unterföhring
+<h2>Kontakt:</h2>
+<table>
+<tbody>
+<tr>
+<td>Telefon (kein Support!):</td>
+<td>015733261155</td>
+</tr>
+<tr>
+<td>E-Mail(kein Support für Kunden!):</td>
+<td>info@it-meindl.de</td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td>Support</td>
+<td>support@wachplan.info</td>
+</tr>
+
+<tr>
+<td>Abuse-Kontakt</td>
+<td> abuse@it-meindl.de
+(für Ermittlungsbehörden und zur Meldung von rechtswidrigen Inhalten auf wachplan.info) </td>
+</tr>
+
+
+</tbody>
+</table>
+<h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
+Tobias Meindl
+Germanenweg 2
+85774 Unterföhring
+
+Quelle: <em><a href="https://www.e-recht24.de">https://www.e-recht24.de</a></em>
+<h1>Haftungsausschluss (Disclaimer)</h1>
+<strong>Haftung für Inhalte</strong>
+
+Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+
+<strong>Haftung für Links</strong>
+
+Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+
+<strong>Urheberrecht</strong>
+
+Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+<h1>Datenschutzerklärung:</h1>
+<strong>Datenschutz</strong>
+
+Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.
+
+Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+
+Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+
+<strong>Datenschutzerklärung für die Nutzung von Google Analytics</strong>
+
+Diese Website nutzt Funktionen des Webanalysedienstes Google Analytics. Anbieter ist die Google Inc. 1600 Amphitheatre Parkway Mountain View, CA 94043, USA. Google Analytics verwendet sog. "Cookies". Das sind Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert.
+
+Im Falle der Aktivierung der IP-Anonymisierung auf dieser Webseite wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt.
+
+Sie können die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website vollumfänglich werden nutzen können. Sie können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem sie das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren: <a href="http://tools.google.com/dlpage/gaoptout?hl=de">http://tools.google.com/dlpage/gaoptout?hl=de</a>
+
+<strong>Auskunft, Löschung, Sperrung</strong>
+
+Sie haben jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung sowie ein Recht auf Berichtigung, Sperrung oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden.
+
+<strong>Kontaktformular</strong>
+
+Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre Angaben aus dem Anfrageformular inklusive der von Ihnen dort angegebenen Kontaktdaten zwecks Bearbeitung der Anfrage und für den Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
+
+
+
+
+Datenschutzerklärung</br>
+
+Diese Datenschutzerklärung dient der Klärung einiger Fragen, die wir nach Bundesdatenschutzgesetz bei Anfrage beantworten müssen.</br>
+
+ 
+Welche Daten werden/sind über mich gespeichert?</br>
+
+Über Deine Person werden die Kontaktdaten, wie die E-Mail-Adresse oder bei zusätzlicher Angabe auch ICQ-Nummer, MSN-Kontakt, Jabber, Google-Talk oder Skype gespeichert. Freiwillig sind das Geburtsdatum, Dein realer Name, Herkunftsland, Größe, Haarfarbe, Augenfarbe, Beruf bzw. Arbeitsstelle, Hobbys, Lebensmotto, Wohnort und Geschlecht. Wir speichern zusätzlich IP-Adressen, Browserkennung und Hostnamen bei jedem Login sowie einigen ausgewählten Aktionen. Deine Aktionen in der Community werden auch gespeichert und sind auf Deinem Profil zu sehen. Wenn Du das nicht möchtest, kannst Du dies in der Verwaltung unter "Community" deaktivieren. Deine Aktionen werden dann nicht mehr angezeigt.
+</br>
+ 
+Wozu werden diese Daten gespeichert?</br>
+
+Die Daten werden einerseits gespeichert, um Kontakt mit Dir aufzunehmen, wenn es Probleme oder Neuigkeiten bezüglich der Webseiten oder der Community gibt – die freiwilligen Daten werden ausschließlich zum Zwecke der Profilierung in der Community gespeichert. IP-Adressen werden gespeichert, um im Falle von strafrechtlichen Ermittlungen der gesellschaftlichen Pflicht nachzukommen, der Ermittlung zuträglich zu sein.
+
+ 
+Woher stammen diese Daten?</br>
+
+Alle bei uns gespeicherten Daten stammen direkt oder indirekt aus Eingabemasken unserer Internetpräsenz. Dies umfasst Anmeldung und private Bereiche. Wir kaufen keine Daten von so genannten "Adressbrokern".
+</br>
+ 
+An wen werden diese Daten herausgegeben?</br>
+
+Die Daten erhält grundsätzlich niemand. In Fällen einer Strafanzeige oder eines sonstigen Rechtsstreites leiten wir allerdings der (Kriminal-)Polizei bzw. dem Staatsanwalt Deine Daten weiter, soweit es unsere Verpflichtung ist. Bei einer Anfrage ohne Eingabe von Rechtsmitteln schließen wir diese Weitergabe allerdings aus.
+</br>
+ 
+Kann ich meine Daten löschen lassen?</br>
+
+Auf Anfrage löschen wir Deine Daten selbstverständlich. 
+</br>
+ 
+AGB</br>
+§ 1</br>
+</br>
+(1) Die Allgemeinen Geschäftsbedingungen regeln das geschäftliche Verhältnis zwischen IT-Meindl, Germaneweg 2, 85774 Unterföhring (im Nachfolgenden IT-Meindl genannt) und dem angemeldeten Benutzer.
+</br>
+(2) Sollte ein Teil dieser allgemeinen Geschäftsbedingungen nicht gültig sein, so berührt dies nicht die Gültigkeit der restlichen Allgemeinen Geschäftsbedingungen.
+</br>
+(3) Abweichungen von diesen Allgemeinen Geschäftsbedingungen bedürfen der Schriftform.
+</br>
+(4) Diese Allgemeinen Geschäftsbedingungen treten mit ihrer Verkündung in Kraft.
+</br>
+(5) Der Gerichtsstand ist München.
+</br>
+ 
+§ 2</br>
+
+(1) Das Angebot von IT-Meindl ist kostenfrei, soweit nicht anders vereinbart oder in diesen Allgemeinen Geschäftsbedingungen geregelt. Die Erbringung der Leistung ist von Seiten des Betreibers lima-city freiwillig, soweit keine Zahlungen seitens des Kunden erfolgt sind.
+</br>
+(2) Es steht IT-Meindl frei, Kunden ohne Ankündigung oder Frist von Teilen oder dem gesamten Angebot auszuschließen, sollte gegen diese Allgemeinen Geschäftsbedingungen verstoßen worden sein.
+</br>
+(3) Anweisungen des Personals (sogenannte Admins & Moderatoren) ist Folge zu leisten; das Personal übt Hausrecht aus.
+</br>
+ 
+§ 3
+</br>
+(1) Der Kunde darf durch seine Aktivitäten weder die Server von IT-Meindl durch enormen Verbrauch von Rechenkapazitäten oder Traffic, noch die Daten anderer Kunden gefährden, noch versuchen durch lima-city zu anderen Computern, Daten oder Computernetzwerken Zutritt zu erlangen.
+</br>
+(2) Der Kunde darf keine rechtlich geschützten oder rechtlich bedenklichen Inhalte auf die Server laden. Dies gilt im besonderen für pornografisches, anarchistisches, gewaltverherrlichendes, beleidigendes, menschenverachtendes oder ähnliches Material, ebenso für Glücksspiel. Der Einsatz von so genannten Proxy-Scripts für den Zugriff auf Internetseiten Dritter ist nicht gestattet.
+</br>
+(3) Der Kunde haftet für Schäden, die Dritte mit gestohlenen Zugangsdaten des Kundenzugangs verursachen.
+</br>
+(4) Sollte der Kunde Daten auf den Servern des Betreibers gespeichert haben, welche gegen 3 Abs. 2 verstoßen, kann der durch diese Daten verursachte Traffic in Rechnung gestellt werden.
+</br>
+(5) Durch den Missbrauch des Webspaces in Form von Verstoß gegen geltendes Recht oder diese Allgemeinen Geschäftsbedingungen entstandene Kosten jeglicher Art werden vom Kunden übernommen.
+</br>
+ 
+§ 4
+</br>
+(1) Die Datensicherung obliegt dem Kunden, soweit nicht anders vereinbart.
+</br>
+(2) Der Kunde stimmt zu, dass lima-city die persönlichen Daten des Kunden elektronisch speichert.
+</br>
+(3) Persönliche Daten und Kennwörter werden nicht ohne ausdrückliche Genehmigung an Dritte weitergegeben.
+</br>
+(4) lima-city übernimmt keine Haftung, sollten persönliche Daten von Kunden durch Unbefugte ausgelesen und/oder weitergegeben werden.
+</br>
+(5) Der Kunde stimmt zu, dass IT-Meindl aktuelle Neuigkeiten in Form von E-Mail an den Kunden schickt.
